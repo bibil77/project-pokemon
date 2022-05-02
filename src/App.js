105e001a9ce3da4366fe22bac853fc3ed6c888d2
benvire <91453870+benvire@users.noreply.github.com>
2022-05-02 11:24:03 +0200

    adding system download

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import TextNb from './components/TextNb';
 import Text from './components/Text';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { findDOMNode } from 'react-dom';
+import $ from 'jquery';
+import html2canvas from 'html2canvas';
 
 // importation d'image
 import firePokemonCard from './assets/img/firePokemonCard.jpg'
@@ -10,8 +13,32 @@ import firePokemonCard from './assets/img/firePokemonCard.jpg'
 function App() {
   const [number, setNumber] = useState(0)
   const [text, setText] = useState(null)
+  
+  //script pour télécharger ce qu'il y a dans la div mainCardDiv
+  const printRef = useRef();
+
+  const downloadImage = async () => {
+    const element = printRef.current;
+    const canvas = await html2canvas(element);
+
+    const data = canvas.toDataURL('image/jpg');
+    const link = document.createElement('a');
+
+    if (typeof link.download === 'string') {
+      link.href = data;
+      link.download = 'image.jpg';
+
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } else {
+      window.open(data);
+    }
+  };
+
   return (
     <div className="App">
+      
       {/* <div>
         <p>{number}</p>
         <TextNb number={number} setNumber={setNumber} placeholder="only number"/>
@@ -30,8 +57,9 @@ function App() {
 
       {/* centre de la page contenant la carte et les boutons de partage, de random et d'enregistrement */}
       <section className='centerSection'>
+
         {/* div contenant la carte */}
-        <div className='mainCarteDiv'>
+        <div className='mainCarteDiv' ref={printRef}>
           <p className='test'>PV {number}</p>
           {/* <img className='imgCenterMainCard' src={firePokemonCard} /> */}
         </div>
@@ -39,6 +67,7 @@ function App() {
         {/* div contenant les boutons de partage et d'enregistrement*/}
         <div className='divCenterBtnDown'>
           div bas centre
+          <button type='button' onClick={downloadImage}>Download as image</button>
         </div>
       </section>
 
